2b4a5ed23d2443d982cee14cc251fc856f8eca1b
Sasha Hayles <sashahayles@yahoo.com>
2019-12-02 23:36:15 -0500

    Add switch styles and JS

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const myCircles = document.querySelectorAll ('.circles #group');
+const layerOne = document.getElementById('layer-one');
+const layerTwo =  document.getElementById('layer-two');
 const mynumbuttons = document.getElementsByClassName('block-2');
 
 //The Functionality
@@ -8,8 +10,17 @@ function loopThrough () {
     setTimeout(flashBg, i * 1000);
   };
 };
+//changelayer
+function changeLayer() {
+  const swap =()=> {
+    layerOne.classList.add('hide');
+    layerTwo.classList.remove('hide');
+  }
+  setTimeout (swap, this.textContent.length * 2 * 1000 );
+};
 
 //The Event Listener
 for (let i = 0; i < myCircles.length; i++) {
-  myCircles[i].addEventListener('click', loopThrough)
+  myCircles[i].addEventListener('click', loopThrough);
+    myCircles[i].addEventListener('click', changeLayer);
 };
