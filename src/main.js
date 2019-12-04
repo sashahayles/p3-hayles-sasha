545d93fec86b8506eaa0731548ff07dbe3f5df50
Sasha Hayles <sashahayles@yahoo.com>
2019-12-04 13:04:39 -0500

    Fix JS loop errors for numbers

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ const layerOne = document.getElementById('layer-one');
 const layerTwo =  document.getElementById('layer-two');
 const mynumbuttons = document.getElementsByClassName('block-2');
 
+const layerThree = document.getElementById ('layer-three');
+const mynumbbuttonsThree = document.getElementsByClassName ('block-3');
+
 //The Functionality
 function loopThrough () {
   const flashBg = () => this.classList.toggle('pulse');
@@ -11,6 +14,12 @@ function loopThrough () {
       setTimeout(flashBg, i * 1000);
     }
   };
+  function loopThroughTwo () {
+    const flashbgTwo = () => this.classList.toggle('pulse');
+      for (let i=0; i < Number(this.textContent); i++) {
+        setTimeout(flashbgTwo, i * 1000);
+      };
+    };
 
 
   //changelayer
@@ -19,7 +28,7 @@ function loopThrough () {
       layerOne.classList.add('hide');
       layerTwo.classList.remove('hide');
     }
-    setTimeout (swap, this.textContent.length * 2 * 1000 );
+    setTimeout (swap, this.textContent.length * 2 * 1000);
   };
 
   //The Event Listener
@@ -27,3 +36,15 @@ function loopThrough () {
     myCircles[i].addEventListener('click', loopThrough);
     myCircles[i].addEventListener('click', changeLayer);
   };
+
+  for (let i = 0; i < mynumbuttons.length; i++) {
+    mynumbuttons[i].addEventListener('click', loopThroughTwo);
+  //  myCircles[i].addEventListener('click', changeLayer);
+  };
+//  function loopThroughTwo () {
+//    const flashBgTwo = () => { this.classList.toggle ('pulse');
+//    const numStringValue = this.firstElementChild.textContent;
+//    for (let i = 0; i < Number(numStringValue) * 2; i++) {
+//      setTimeout (flashbgTwo, i * 1000);
+//    }
+//  };
