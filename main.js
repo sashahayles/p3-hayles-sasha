0b643d0370cb17e0979335bdd549999b83bfe04c
Sasha Hayles <sashahayles@yahoo.com>
2019-12-03 19:30:23 -0500

    Attempt to fix JS number switch error

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const myCircles = document.querySelectorAll ('.circles #group');
 const layerOne = document.getElementById('layer-one');
+
 const layerTwo =  document.getElementById('layer-two');
 const mynumbuttons = document.getElementsByClassName('block-2');
 
 //The Functionality
 function loopThrough () {
-  const flashBg = () => this.classList.toggle('pulse');
+  const flashBg = () => {
+     this.classList.toggle('pulse');
+   };
   for (let i=0; i < this.textContent.length * 2; i++) {
     setTimeout(flashBg, i * 1000);
-  };
+  }
+};
+
+function loopThroughTwo () {
+  const flashBgTwo = () => {
+     this.classList.toggle ('pulse');
+   };
+  for (let i = 0; i < Number("3 4 5") this.textContent * 2; i++) {
+    setTimeout (flashbgTwo, i * 1000);
+  }
 };
 //changelayer
 function changeLayer() {
@@ -23,4 +35,6 @@ function changeLayer() {
 for (let i = 0; i < myCircles.length; i++) {
   myCircles[i].addEventListener('click', loopThrough);
   myCircles[i].addEventListener('click', changeLayer);
+  myCircles[i].addEventListener('click', loopThroughTwo);
+
 };
